9024731d4ca8af74dadc1b9f3d8eaf6ff4d5fdf5
yannacone <yannacone@me.com>
2016-08-31 07:51:23 +0000

    Added usernames, username routing, profile pages, username sign-ups, etc...

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,12 @@ Rails.application.routes.draw do
   
   # Define Root URL
   root 'pages#index'
-  
-  
   # Define Routes for Pages
-  get 'home' => 'pages#home'
+  get '/home' => 'pages#home'
 
-  get 'profile' => 'pages#profile'
+  get '/user/:id' => 'pages#profile'
 
-  get 'explore' => 'pages#explore'
+  get '/explore' => 'pages#explore'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
